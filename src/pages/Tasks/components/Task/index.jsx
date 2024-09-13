761fb58a87644cf79d2e46595fac72ce9ed1eecb
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-09-13 11:06:28 -0300

    Updating status

--- src/pages/Tasks/components/Task/index.jsx
+++ src/pages/Tasks/components/Task/index.jsx
@@ -1,8 +1,24 @@
 import styled from './styles.module.sass'
 import editIcon from '/edit-icon.png'
 import deleteIcon from '/delete-icon.png'
+import { useState } from 'react'
+import { requestHook } from '../../../../hooks/request.hook'
 
 export const Task = ({ title, responsible, description, checkedButton, deleteModal, updateModal, id }) => {
+    const [status, setStatus] = useState(0);
+    const { handleRequest } = requestHook()
+
+    async function handleStatus(sttsId){
+        try {
+            let stts = ({ "task_id": id, "status_id": sttsId })
+            const response = await handleRequest(`/task/updateStatus`, 'PUT', stts)
+            console.log(response, "QQQQQQQQQQQQQQWWWWWWWWWWWWWWWW")
+            window.location.reload();
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return <>
         <div className={styled.task}>
             <div className={styled.header}>
@@ -17,15 +33,15 @@ export const Task = ({ title, responsible, description, checkedButton, deleteMod
 
             <div className={styled.buttonGroup}>
                 <div className={styled.button}>
-                    <input type="radio" checked={ checkedButton === 1 } readOnly></input>
+                    <input type="radio" checked={ checkedButton === 1 } onClick={() => handleStatus(1)} ></input>
                     <label>A fazer</label>
                 </div>
                 <div className={styled.button}>
-                    <input type="radio" checked={ checkedButton === 2 } readOnly></input>
+                    <input type="radio" checked={ checkedButton === 2 } onClick={() => handleStatus(2)} ></input>
                     <label>Em andamento</label>
                 </div>
                 <div className={styled.button}>
-                    <input type="radio" checked={ checkedButton === 3 } readOnly></input>
+                    <input type="radio" checked={ checkedButton === 3 } onClick={() => handleStatus(3)} ></input>
                     <label>Finalizado</label>
                 </div>
             </div>
